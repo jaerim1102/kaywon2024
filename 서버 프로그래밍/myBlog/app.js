571362720634e1e0c1270de1f57a5af88b65159e
jaerim <jaerim1102@naver.com>
2024-11-15 09:45:46 +0900

    1115: 서버프로그래밍 환경구성

--- 서버 프로그래밍/myBlog/app.js
+++ 서버 프로그래밍/myBlog/app.js
@@ -1,11 +1,18 @@
-require("dotenv").config(); // .env에 있는 변수 가져오기
+require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
-const port = process.env.PORT || 3000; // .env에 PORT가 없으면 3000번 포트 사용
+const port = process.env.PORT || 3000;
 
-// 루트(/) 경로로 요청하면 routes\main.js의 라우트 사용
+//레이아웃과 뷰 엔진 설정
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// 정적 파일
+app.use(express.static("public"));
+
 app.use("/", require("./routes/main"));
 
 app.listen(port, () => {
